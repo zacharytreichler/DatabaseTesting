cd7d88c88a7acdede8ea38be9d8eafa2056e99be
zdt227 <zdt227@lehigh.edu>
2025-06-03 10:54:53 +0200

    3/6/2025 10:54, Zachary Treichler. Added detailed commenting to the makeTable method in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -87,7 +87,7 @@ public class App {
      * Method Creation Date (DD/MM/YYYY, Developer Name): 28/5/2025, Zachary Treichler
      * Most Recent Change (DD/MM/YYYY, Developer Name): 3/6/2025, Zachary Treichler
      * Method Description: This method is used to generate tables in a database. It establishes a connection using a database URI, and then executes a prepared statement based
-     * on the query variable that was passed into the method. This method uses try with resources, so the connection closes if any error occurs to prevent zombie instances. 
+     * on the query variable that was passed into the method. This method uses try with resources, so the connection closes if any error occurs to prevent zombie instances.
      * Functions Using This Method: main method
      * Description of Variables:
      * @param query - Used to store the SQL query that will be executed by this method
